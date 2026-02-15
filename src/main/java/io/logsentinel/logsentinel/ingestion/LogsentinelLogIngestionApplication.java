00b42ea34da73ca9b5d11da6f7a1ee12dea25df4
Harsh-Codes-382 <harshkumar3826@gmail.com>
2026-02-15 07:38:42 +0530

    Initial Commit for Log Ingestion Service

---
+++ src/main/java/io/logsentinel/logsentinel/ingestion/LogsentinelLogIngestionApplication.java
@@ -0,0 +1,14 @@
+package io.logsentinel.logsentinel.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsentinelLogIngestionApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LogsentinelLogIngestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/logsentinel/logsentinel/ingestion/LogsentinelLogIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.logsentinel.logsentinel.ingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsentinelLogIngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
